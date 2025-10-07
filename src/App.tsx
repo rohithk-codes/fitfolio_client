761d9516c2419afa116c2cfd61885605e8fc34cd
Rohith krishnan <rohithkrishnan997@gmail.com>
2025-10-07 23:35:32 +0530

    First rendering

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+
+export function SkeletonCard() {
+  return (
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1 className="text-green-400 m-4 font-bold">Welcome to Fit_Folio</h1>
+
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <SkeletonCard />
+        <ModeToggle />
+      </ThemeProvider>
+    </div>
+  );
+};
+
+export default App;
